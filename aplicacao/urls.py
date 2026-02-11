4897c80c22f6c25bcff9203bfb36bae3615df271
Gabriel <claudiogabrielpickciuspauleti@gmail.com>
2026-02-11 18:18:19 -0300

    fim da aula

--- aplicacao/urls.py
+++ aplicacao/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import index
+from .views import contato
 
 urlpatterns = [
 
-    path('', index)
+    path('', index),
+    path('contato', contato),
+
 
 ]
--- aplicacao/views.py
+++ aplicacao/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    context = {'curso': 'Desenvolvimento de Sistemas'}
+    return render(request, 'index.html', context)
+
+def contato(request):
+    context = {'nome': 'Instituto Federal de SC', 'telefone': '(47) 3363-5251', 'email': 'contato@ifsc.edu.br'}
+    return render(request, 'contato.html', context)
