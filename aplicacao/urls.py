a9552efa467f66cf70afe538315855068c00e616
Gabriel <claudiogabrielpickciuspauleti@gmail.com>
2026-02-25 17:36:12 -0300

    me perdi

--- aplicacao/urls.py
+++ aplicacao/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from .views import index
-from .views import contato
+from .views import contato, produto, cadastrarProduto, salvarProduto
 
 urlpatterns = [
 
     path('', index),
-    path('contato', contato),
+    path('contato', contato, name="urlcontato"),
+    path('produto', produto, name="urlproduto"),
+    path('cadastrarProduto', cadastrarProduto, name= 'urlcadastrarProduto')
+    path('salvarProduto', salvarProduto, name= 'urlsalvarProduto' )
+
 
 
 ]
--- aplicacao/views.py
+++ aplicacao/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
+from django.http import HttpResponse
+from .models import Produto
 
 def index(request):
     context = {'curso': 'Desenvolvimento de Sistemas'}
@@ -7,3 +9,30 @@ def index(request):
 def contato(request):
     context = {'nome': 'Instituto Federal de SC', 'telefone': '(47) 3363-5251', 'email': 'contato@ifsc.edu.br'}
     return render(request, 'contato.html', context)
+
+def produto(request):
+    produtos = Produto.objects.all()
+    context = {'produtos': produtos}
+    return render(request, "produto.html", context)
+
+def cadastrarProduto(request):
+    return render(request, "cadastrarProduto.html")
+
+def salvarProduto(request):
+    thisnome = requeste.POST.get('txtNome')
+    thispreco = request.POST.get('txtPreco')
+    thisqtd = request.POST.get('txtQtd')
+    thisdata = request.POST.get('txtData')
+    thisdescricao = request.POST.get('txtDescricao')
+
+    print(thisnome)
+
+    produto = Produto(
+        nome = thisnome
+        preco = thispreco
+        data = thisdata
+        descricao = thisdescricao 
+    )
+
+    produto.save()
+    return redirect('urlproduto')
