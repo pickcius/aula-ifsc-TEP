f127085b0bf049bc865bac9d2e3420e37bd8389e
Gabriel <claudiogabrielpickciuspauleti@gmail.com>
2026-02-25 15:10:00 -0300

    tabela produtos funcionando

--- aplicacao/models.py
+++ aplicacao/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Produto(models.Model):
+    nome = models.CharField(max_length=100, null=True, blank=True)
+    preco = models.DecimalField(decimal_places=2, max_digits=10, null=True, blank=True)
+    qtd = models.IntegerField(null=True, blank=True)
+    descricao = models.CharField(max_length=250, null=True, blank= True)
+    data = models.DateField(null=True, blank= True)
+
