751e2f3d2996b8b83668e4fcd9576ef7b3ea304d
Gabriel <claudiogabrielpickciuspauleti@gmail.com>
2026-03-18 17:59:03 -0300

    por aqui safe

---
+++ static/js/script.js
@@ -0,0 +1,29 @@
+(() => {
+  const btnCloseMenu = document.querySelector('.button-close-menu');
+  const btnShowMenu = document.querySelector('.button-show-menu');
+  const menuContainer = document.querySelector('.menu-container');
+
+  const btnShowMenuVisibleClass = 'button-show-menu-visible';
+  const menuHiddenClass = 'menu-hidden';
+
+  const closeMenu = () => {
+      btnShowMenu.classList.add(btnShowMenuVisibleClass);
+      menuContainer.classList.add(menuHiddenClass);
+  }
+
+  const showMenu = () => {
+      btnShowMenu.classList.remove(btnShowMenuVisibleClass);
+      menuContainer.classList.remove(menuHiddenClass);
+  }
+
+  if(btnCloseMenu){
+      btnCloseMenu.removeEventListener('click', closeMenu);
+      btnCloseMenu.addEventListener('click', closeMenu);
+  }
+
+  if(btnShowMenu){
+      btnCloseMenu.removeEventListener('click', showMenu);
+      btnShowMenu.addEventListener('click', showMenu);
+  }
+
+})();
