4168e73c59f9067ec344e17c18f45c04919bd2d6
Gabriel <claudiogabrielpickciuspauleti@gmail.com>
2026-03-25 17:32:36 -0300

    erro

--- aplicacao/admin.py
+++ aplicacao/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
 
-from .models import Produto
+from .models import Produto, Perfil, Venda, ItemVenda
 
 admin.site.register(Produto)
+
+@admin.register(Produto)
+class ProdutoAdm(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'preco', 'qtde', 'data')
+    list_display_links = ('nome', )
+    search_fields = ('nome', )
+    list_filter = ('preco', 'qtde')
+
+@admin.register(Perfil)
+class PerfilAdm(admin.ModelAdmin):
+    list_display = ('id', 'client', 'telefone', 'cidade')
+    searchfields = ('cliente__username',)
+
+@admin.register(Venda)
+    list_display = ('id', 'cliente', 'data')
+    list_filter = ('data',)
+
+@admin.register(ItemVenda)
+class ItemVendaAdm(admin.ModelAdmin):
+    list_display = ('id', 'venda', 'produto', 'qtde')
+    
+
--- aplicacao/models.py
+++ aplicacao/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Produto(models.Model):
     nome = models.CharField(max_length=100, null=True, blank=True)
@@ -9,4 +10,30 @@ class Produto(models.Model):
     imagem = models.ImageField(upload_to='produtos/', null=True, blank=True)
 
     def __str__(self):
-        return self.nome
+    return self.nome or "Produto sem nome"
+
+class Perfil(models.Model):
+    telefone = models.CharField(max_length=25, null=True, blank=True,)
+    rua = models.CharField(max_length=50, null=True, blank=True)
+    numero = models.CharField(null=True, blank=True)
+    bairro = models.CharField(max_length=50, null=True, blank=True)
+    cidade = models.CharField(max_length=50, null=True, blank=True)
+    complemento = models.CharField(max_length=100, null=True, blank=True)
+    cep = models.CharField(max_length=50, null=True, blank=True)
+    cliente = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Perfil de {self.cliente.username}'
+
+class Venda(models.Model):
+    data = models.DateField(auto_now_add=True)
+    cliente = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Venda {self.id} - {self.data}'
+
+class ItemVenda(models.Model):
+    venda = models.ForeignKey(Venda, on_delete=models.CASCADE)
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    qtde= models.ImageField(null=True, blank=True)
+
